133b0ca7241ee1ebce7269444e649d573470def9
Brooke Chalmers <breq@breq.dev>
2024-09-06 22:30:39 -0400

    create/delete iframes for walk

--- src/Walk.tsx
+++ src/Walk.tsx
@@ -11,58 +11,87 @@ export default function Walk({
 }) {
   const posn = useRef({ x: 0, y: 0 });
 
-  const tl = useRef<HTMLIFrameElement>(null);
-  const tr = useRef<HTMLIFrameElement>(null);
-  const bl = useRef<HTMLIFrameElement>(null);
-  const br = useRef<HTMLIFrameElement>(null);
+  const parent = useRef<HTMLDivElement>(null);
+  const frames = useRef<Record<string, HTMLIFrameElement | null>>({});
+
+  const createFrame = useCallback((src: string) => {
+    const newFrame = document.createElement("iframe");
+    newFrame.src = "https://" + src;
+    newFrame.style.width = `${FRAME_SIZE}px`;
+    newFrame.style.height = `${FRAME_SIZE}px`;
+    newFrame.style.position = "absolute";
+    newFrame.style.border = "8px black";
+    newFrame.style.backgroundColor = "white";
+    return newFrame;
+  }, []);
 
   const moveFrames = useCallback(() => {
     const x_frac = posn.current.x - Math.floor(posn.current.x);
     const y_frac = posn.current.y - Math.floor(posn.current.y);
 
-    // keep all 4 iframes in view
-    tl.current!.style.top = 0 - y_frac * FRAME_SIZE + "px";
-    tl.current!.style.left = 0 - x_frac * FRAME_SIZE + "px";
-    tr.current!.style.top = 0 - y_frac * FRAME_SIZE + "px";
-    tr.current!.style.left = FRAME_SIZE - x_frac * FRAME_SIZE + "px";
-    bl.current!.style.top = FRAME_SIZE - y_frac * FRAME_SIZE + "px";
-    bl.current!.style.left = 0 - x_frac * FRAME_SIZE + "px";
-    br.current!.style.top = FRAME_SIZE - y_frac * FRAME_SIZE + "px";
-    br.current!.style.left = FRAME_SIZE - x_frac * FRAME_SIZE + "px";
+    // figure out what 4 frames should be in view
+    const tl =
+      rooms[`${Math.floor(posn.current.x)},${Math.floor(posn.current.y)}`];
+    const tr =
+      rooms[`${Math.floor(posn.current.x) + 1},${Math.floor(posn.current.y)}`];
+    const bl =
+      rooms[`${Math.floor(posn.current.x)},${Math.floor(posn.current.y) + 1}`];
+    const br =
+      rooms[
+        `${Math.floor(posn.current.x) + 1},${Math.floor(posn.current.y) + 1}`
+      ];
 
-    // load the pages
-    function updateFrame(frame: HTMLIFrameElement, src: string) {
-      if (!frame.src || new URL(frame.src).origin !== new URL(src).origin) {
-        frame.src = src;
-      }
+    // create iframes if needed
+    if (!frames.current[tl.domain]) {
+      console.log(`Creating frame for ${tl.domain}`);
+      frames.current[tl.domain] = createFrame(tl.domain);
+      parent.current?.appendChild(frames.current[tl.domain]!);
+    }
+    if (!frames.current[tr.domain]) {
+      console.log(`Creating frame for ${tr.domain}`);
+      frames.current[tr.domain] = createFrame(tr.domain);
+      parent.current?.appendChild(frames.current[tr.domain]!);
+    }
+    if (!frames.current[bl.domain]) {
+      console.log(`Creating frame for ${bl.domain}`);
+      frames.current[bl.domain] = createFrame(bl.domain);
+      parent.current?.appendChild(frames.current[bl.domain]!);
+    }
+    if (!frames.current[br.domain]) {
+      console.log(`Creating frame for ${br.domain}`);
+      frames.current[br.domain] = createFrame(br.domain);
+      parent.current?.appendChild(frames.current[br.domain]!);
     }
 
-    updateFrame(
-      tl.current!,
-      "https://" +
-        rooms[`${Math.floor(posn.current.x)},${Math.floor(posn.current.y)}`]
-          ?.domain
-    );
-    updateFrame(
-      tr.current!,
-      "https://" +
-        rooms[`${Math.floor(posn.current.x) + 1},${Math.floor(posn.current.y)}`]
-          ?.domain
-    );
-    updateFrame(
-      bl.current!,
-      "https://" +
-        rooms[`${Math.floor(posn.current.x)},${Math.floor(posn.current.y) + 1}`]
-          ?.domain
-    );
-    updateFrame(
-      br.current!,
-      "https://" +
-        rooms[
-          `${Math.floor(posn.current.x) + 1},${Math.floor(posn.current.y) + 1}`
-        ]?.domain
-    );
-  }, [rooms]);
+    // keep all 4 iframes in view
+    frames.current[tl.domain]!.style.top = 0 - y_frac * FRAME_SIZE + "px";
+    frames.current[tl.domain]!.style.left = 0 - x_frac * FRAME_SIZE + "px";
+    frames.current[tr.domain]!.style.top = 0 - y_frac * FRAME_SIZE + "px";
+    frames.current[tr.domain]!.style.left =
+      FRAME_SIZE - x_frac * FRAME_SIZE + "px";
+    frames.current[bl.domain]!.style.top =
+      FRAME_SIZE - y_frac * FRAME_SIZE + "px";
+    frames.current[bl.domain]!.style.left = 0 - x_frac * FRAME_SIZE + "px";
+    frames.current[br.domain]!.style.top =
+      FRAME_SIZE - y_frac * FRAME_SIZE + "px";
+    frames.current[br.domain]!.style.left =
+      FRAME_SIZE - x_frac * FRAME_SIZE + "px";
+
+    // remove frames that are out of view
+    for (const key in frames.current) {
+      if (
+        key === tl.domain ||
+        key === tr.domain ||
+        key === bl.domain ||
+        key === br.domain
+      ) {
+        continue;
+      }
+      console.log(`Removing frame for ${key}`);
+      parent.current?.removeChild(frames.current[key]!);
+      delete frames.current[key];
+    }
+  }, [rooms, createFrame]);
 
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
@@ -88,7 +117,7 @@ export default function Walk({
 
   useLayoutEffect(() => {
     moveFrames();
-  }, [moveFrames]);
+  }, [createFrame, moveFrames]);
 
   const dragging = useRef(false);
 
@@ -100,48 +129,8 @@ export default function Walk({
           width: `${FRAME_SIZE}px`,
           height: `${FRAME_SIZE}px`,
         }}
+        ref={parent}
       >
-        <iframe
-          ref={tl}
-          style={{
-            width: `${FRAME_SIZE}px`,
-            height: `${FRAME_SIZE}px`,
-            position: "absolute",
-            border: "8px black",
-            backgroundColor: "white",
-          }}
-        />
-        <iframe
-          ref={tr}
-          style={{
-            width: `${FRAME_SIZE}px`,
-            height: `${FRAME_SIZE}px`,
-            position: "absolute",
-            border: "8px black",
-            backgroundColor: "white",
-          }}
-        />
-        <iframe
-          ref={bl}
-          style={{
-            width: `${FRAME_SIZE}px`,
-            height: `${FRAME_SIZE}px`,
-            position: "absolute",
-            border: "8px black",
-            backgroundColor: "white",
-          }}
-        />
-        <iframe
-          ref={br}
-          style={{
-            width: `${FRAME_SIZE}px`,
-            height: `${FRAME_SIZE}px`,
-            position: "absolute",
-            border: "8px black",
-            backgroundColor: "white",
-          }}
-        />
-
         <div
           className="absolute inset-0 border-2 border-black z-10"
           onMouseDown={() => {
